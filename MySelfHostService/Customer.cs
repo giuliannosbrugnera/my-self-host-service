ecb572bf585d12ef731884c45334851c7136b54a
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-12 15:29:40 -0300

    Using contracts provided by WCF to decorate the interface and the class

--- MySelfHostService/Customer.cs
+++ MySelfHostService/Customer.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Runtime.Serialization;
 
 namespace MySelfHostService
 {
+    [DataContract]
     public class Customer
     {
+        [DataMember]
         public int CustomerId { get; set; }
+
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember]
         public string Email { get; set; }
+
         public string CreditCardNumber { get; set; }
     }
 }
--- MySelfHostService/ICustomer.cs
+++ MySelfHostService/ICustomer.cs
@@ -1,9 +1,17 @@
-﻿namespace MySelfHostService
+﻿using System.ServiceModel;
+
+namespace MySelfHostService
 {
+    [ServiceContract]
     public interface ICustomer
     {
+        [OperationContract]
         Customer CreateCustomer(int customerId, string name, string email);
+
+        [OperationContract]
         Customer GetCustomerByName(string customerName);
+
+        [OperationContract]
         Customer[] GetAllCustomers();
     }
 }
